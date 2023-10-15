6831b66f2d2a5f74f137eb6b61a51f2e00cd7b2b
BhavitJain07 <136237680+BhavitJain07@users.noreply.github.com>
2023-10-15 22:02:18 +0530

    add new file

---
+++ Test_Program/P_7.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a = 3;
+    switch (a)
+    {
+    case 2:
+        printf("Value is 2");
+        break;
+
+    case 3:
+        printf("Value is 3");
+        break;
+
+    default:
+        break;
+    }
+}
